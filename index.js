5e09fa0574ed546830273d65929013f4b40c98b5
marie <marie.jones@etudiant.univ-lille1.fr>
2017-07-31 15:35:45 +0200

    Debug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Raspi = require('raspi-io');
 const five = require('johnny-five');
 const board = new five.Board({
-    io: new Raspi()
+    io: new raspi()
 });
 
 // board.on('ready', () => {
