aff1e5db9ba4ca8265c86d878842dc6c08d09b9e
marie <marie.jones@etudiant.univ-lille1.fr>
2017-07-31 15:12:21 +0200

    Led declaration

---
+++ index.js
@@ -0,0 +1,13 @@
+const Raspi = require('raspi-io');
+const five = require('johnny-five');
+const board = new five.Board({
+    io: new Raspi()
+});
+
+board.on('ready', () => {
+
+    let led = new five.Led(6);
+    // led.pulse();
+    // this.wait(2000);
+    // led.stop().off();
+});
