46defb56552ed767d7986b40c1765822445058a9
marie <marie.jones@etudiant.univ-lille1.fr>
2017-07-31 15:31:25 +0200

    Debug

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const Raspi = require('raspi-io');
-const five = require('johnny-five');
-const board = new five.Board({
-    io: new Raspi()
-});
+// const five = require('johnny-five');
+// const board = new five.Board({
+//     io: new Raspi()
+// });
 
-board.on('ready', () => {
+// board.on('ready', () => {
 
-    console.log("ready");
-    //let led = new five.Led(6);
-    // led.pulse();
-    // this.wait(2000);
-    // led.stop().off();
-});
+//     console.log("ready");
+//     //let led = new five.Led(6);
+//     // led.pulse();
+//     // this.wait(2000);
+//     // led.stop().off();
+// });
